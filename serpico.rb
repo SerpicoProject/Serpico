f6d77e386ad2d6630500324aa0703c836ecb2ff3
BuffaloWill <wvandevanter@gmail.com>
2014-07-28 16:25:21 -0400

    added support to edit a template

--- serpico.rb
+++ serpico.rb
@@ -645,18 +645,71 @@ post '/admin/templates/add' do
     end
 end
 
-
 # Manage Templated Reports
 get '/admin/templates/:id/edit' do
     redirect to("/no_access") if not is_administrator?
 
-    @admin = true
-
-    # Query for all Findings
+    @admind = true
     @template = Xslt.first(:id => params[:id])
 
-    haml :add_template, :encode_html => true
- end
+    haml :edit_template, :encode_html => true
+end
+
+# Manage Templated Reports
+post '/admin/templates/edit' do
+    redirect to("/no_access") if not is_administrator?
+
+    @admin = true
+    template = Xslt.first(:id => params[:id])
+
+    xslt_file = template.xslt_location
+
+    # reject if the file is above a certain limit
+    if params[:file][:tempfile].size > 100000000
+      return "File too large. 10MB limit"
+    end
+
+    docx = "./templates/#{rand(36**36).to_s(36)}.docx"
+    File.open(docx, 'wb') {|f| f.write(params[:file][:tempfile].read) }
+
+    error = false
+    detail = ""
+    begin
+	    xslt = generate_xslt(docx)
+    rescue ReportingError => detail
+        error = true
+    end
+
+    if error
+        "The report template you uploaded threw an error when parsing:<p><p> #{detail.errorString}"
+    else
+
+    	# open up a file handle and write the attachment
+	    File.open(xslt_file, 'wb') {|f| f.write(xslt) }
+
+	    # delete the file data from the attachment
+	    datax = Hash.new
+	    # to prevent traversal we hardcode this
+	    datax["docx_location"] = "#{docx}"
+	    datax["xslt_location"] = "#{xslt_file}"
+	    datax["description"] = 	params[:description]
+	    datax["report_type"] = params[:report_type]
+	    data = url_escape_hash(datax)
+	    data["finding_template"] = params[:finding_template] ? true : false
+	    data["status_template"] = params[:status_template] ? true : false
+
+	    @current = Xslt.first(:report_type => data["report_type"])
+
+	    if @current
+		    @current.update(:xslt_location => data["xslt_location"], :docx_location => data["docx_location"], :description => data["description"])
+	    else
+		    @template = Xslt.new(data)
+		    @template.save
+	    end
+
+	    redirect to("/admin/templates")
+    end
+end
 
 #####
 # Reporting Routes
