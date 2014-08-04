999f38a32228b8f20874b6d2fc6f354327b04767
BuffaloWill <wvandevanter@gmail.com>
2014-08-04 18:31:57 -0400

    updated report templates and added findings

--- scripts/first_time.rb
+++ scripts/first_time.rb
@@ -9,25 +9,26 @@ require 'rubygems'
 require './model/master.rb'
 require './helpers/xslt_generation'
 require 'openssl'
+require 'json'
 
 userx = User.first
 
 # If there are no users, create a first user
 if !userx
 	puts "No users in the database, creating a first user. \n"
-	
+
 	puts "Please enter username (default: administrator):  "
 	username = gets.chomp
 	username = "administrator" if username == ""
 
 	puts "Generating random password and adding the Administrator with username #{username}..."
-	
+
 	password = rand(36**10).to_s(36)
 
 	exists = User.first(:username => username)
-	
+
 	if exists
-		puts "That username already exists. Please use reset_pw.rb to reset a password"		
+		puts "That username already exists. Please use reset_pw.rb to reset a password"
 	else
 		user = User.new
 		user.username = username
@@ -44,15 +45,40 @@ else
 	puts "Skipping username creation (users exist), please use the create_user.rb script to add a user."
 end
 
+puts "Would you like to initialize the database with templated findings? (Y/n)"
+
+find_i = gets.chomp
+if (find_i == "" or find_i.downcase == "y" or find_i.downcase == "yes")
+    puts "Importing Templated Findings template_findings.json..."
+
+    file = File.new('./templates/template_findings.json',"rb")
+    json = ""
+    while(line_j = file.gets)
+        json = json + line_j
+    end
+    line = JSON.parse(json)
+
+    line.each do |j|
+        j["id"] = nil
+
+        finding = TemplateFindings.first(:title => j["title"])
+
+        j["approved"] = true
+        f = TemplateFindings.first_or_create(j)
+        f.save
+    end
+else
+    puts "Skipping templated finding import. Use the UI to import templated findings."
+end
+
 # add the Default templates into the DB
 templates = Xslt.first
 
 if !templates
-	puts "Adding the Serpico Default Report Template"
-
+	puts "Adding the Default DREAD Report Template"
 	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
 	docx = "./templates/Serpico - Report.docx"
-	
+
 	xslt = generate_xslt(docx)
 	if xslt =~ /Error file DNE/
 		return "ERROR!!!!!!"
@@ -60,7 +86,7 @@ if !templates
 
 	# open up a file handle and write the attachment
 	File.open(xslt_file, 'wb') {|f| f.write(xslt) }	
-	
+
 	# delete the file data from the attachment
 	datax = Hash.new
 	datax["docx_location"] = "#{docx}"
@@ -69,6 +95,27 @@ if !templates
 	datax["report_type"] = "Default Template"
 	report = Xslt.new(datax)
 	report.save
+
+    puts "Adding the Default Generic Risk Scoring Report Template"
+    xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
+    docx = "./templates/Serpico - GenericRiskScoring.docx"
+
+    xslt = generate_xslt(docx)
+    if xslt =~ /Error file DNE/
+        return "ERROR!!!!!!"
+    end
+
+    # open up a file handle and write the attachment
+    File.open(xslt_file, 'wb') {|f| f.write(xslt) } 
+
+    # delete the file data from the attachment
+    datax = Hash.new
+    datax["docx_location"] = "#{docx}"
+    datax["xslt_location"] = "#{xslt_file}" 
+    datax["description"] =  "Generic Risk Scoring Report"
+    datax["report_type"] = "Default Template - Generic Risk Scoring"
+    report = Xslt.new(datax)
+    report.save
 
 	puts "Adding the Serpico Default Finding Template"
 
@@ -154,5 +201,3 @@ end
 File.open("./key.pem", "w") do |f|
   f.write key.to_pem
 end
-
-
