8c1078e029409cf47ef873e29b6b136bafbc7160
BuffaloWill <wvandevanter@gmail.com>
2015-10-01 19:42:10 -0400

    fix infinite redirect loop

--- serpico.rb
+++ serpico.rb
@@ -59,8 +59,9 @@ error do
 end
 
 # Run a session check on every route
-["/info","/reports/*","/report/*","/","/login","/logout","/admin/*","/master/*","/mapping/*"].each do |path|
+["/info","/reports/*","/report/*","/","/logout","/admin/*","/master/*","/mapping/*"].each do |path|
     before path do
+        next if request.path_info == "/reports/list"
         redirect '/reports/list' unless valid_session?
     end
 end
