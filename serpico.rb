618ddc1dd49229e90dc5f2609d38f2ef676431fa
BuffaloWill <wvandevanter@gmail.com>
2016-07-29 09:01:40 -0400

    support for issue138

--- serpico.rb
+++ serpico.rb
@@ -368,6 +368,14 @@ get '/admin/config' do
     redirect to("/no_access") if not is_administrator?
 
     @config = config_options
+    if config_options["cvss"]
+        @scoring = "cvss"
+    elsif config_options["dread"]
+        @scoring = "dread"
+    else
+        @scoring = "default"
+    end
+
     haml :config, :encode_html => true
 end
 
@@ -385,14 +393,23 @@ post '/admin/config' do
     config_options["ldap"] = params["ldap"] ? true : false
     config_options["ldap_domain"] = params["ldap_domain"]
     config_options["ldap_dc"] = params["ldap_dc"]
-    config_options["dread"] = params["dread"] ? true : false
-    config_options["cvss"] = params["cvss"] ? true : false
     config_options["burpmap"] = params["burpmap"] ? true : false
     config_options["nessusmap"] = params["nessusmap"] ? true : false
     config_options["logo"] = params["logo"]
     config_options["auto_import"] = params["auto_import"] ? true : false
     config_options["chart"] = params["chart"] ? true : false
     config_options["threshold"] = params["threshold"]
+
+    if params["risk_scoring"] == "CVSS"
+        config_options["dread"] = false
+        config_options["cvss"] = true
+    elsif params["risk_scoring"] == "DREAD"
+        config_options["dread"] = true
+        config_options["cvss"] = false
+    else
+        config_options["dread"] = false
+        config_options["cvss"] = false
+    end
 
     File.open("./config.json","w") do |f|
       f.write(JSON.pretty_generate(config_options))
