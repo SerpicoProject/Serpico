ad0aca8f4de6cf9744cda44059362f2ac4bc19d5
BuffaloWill <wvandevanter@gmail.com>
2016-10-03 20:09:24 -0400

    implemented https://github.com/SerpicoProject/Serpico/issues/187

--- routes/basic.rb
+++ routes/basic.rb
@@ -78,6 +78,44 @@ post '/info' do
     redirect to("/info")
 end
 
+# Handles password reset
+get '/reset' do
+    redirect '/reports/list' unless valid_session?
+
+    haml :reset, :encode_html => true
+end
+
+# Handles the consultant information settings
+post '/reset' do
+    redirect '/reports/list' unless valid_session?
+
+    # grab the user info
+    user = User.first(:username => get_username)
+
+    # check if they are an LDAP user
+    if user.auth_type != "Local"
+        return "You are an LDAP user. You cannot change your password."
+    end
+
+    # check if the password is greater than 3 chars. legit complexity rules =/
+    #   TODO add password complexity requirements
+    if params[:new_pass].size < 4
+        return "Srsly? Your password must be greater than 3 characters."
+    end
+
+    if params[:new_pass] != params[:new_pass_confirm]
+        return "New password does not match."
+    end
+
+    if !(User.authenticate(user.username,params[:old_pass]))
+        return "Old password is incorrect."
+    end
+
+    user.update(:password => params[:new_pass])
+    @message = "success"
+    haml :reset, :encode_html => true
+end
+
 post '/login' do
     user = User.first(:username => params[:username])
 
