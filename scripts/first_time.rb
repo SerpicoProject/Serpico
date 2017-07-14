2ee60c70525b55f6050f12c5aabd6d9702d2d9b2
ccammilleri <caseycam@gmail.com>
2017-07-14 11:08:23 -0500

    support non-interactive/quiet setup for testing

--- scripts/first_time.rb
+++ scripts/first_time.rb
@@ -2,6 +2,20 @@ require './model/master.rb'
 require './helpers/xslt_generation'
 require 'openssl'
 require 'json'
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+	opts.banner = "Usage: first_time.rb [options]"
+	opts.on('-q', '--quiet', 'Quiet mode') do |quiet|
+		options[:quiet] = true;
+	end
+	opts.on('-h', '--help', 'Displays Help') do 
+		puts opts
+		exit
+	end
+end.parse!
+
 
 userx = User.first
 
@@ -10,7 +24,11 @@ if !userx
 	puts "No users in the database, creating a first user. \n"
 
 	puts "Please enter username (default: administrator):  "
-	username = gets.chomp
+	if options[:quiet]
+		username = "administrator"
+	else
+		username = gets.chomp
+	end
 	username = "administrator" if username == ""
 
 	puts "Generating random password and adding the Administrator with username #{username}..."
@@ -39,28 +57,34 @@ end
 
 puts "Would you like to initialize the database with templated findings? (Y/n)"
 
-find_i = gets.chomp
-if (find_i == "" or find_i.downcase == "y" or find_i.downcase == "yes")
-    puts "Importing Templated Findings template_findings.json..."
+if options[:quiet] == nil
+	find_i = gets.chomp
 
-    file = File.new('./templates/template_findings.json',"rb")
-    json = ""
-    while(line_j = file.gets)
-        json = json + line_j
-    end
-    line = JSON.parse(json)
+	if (find_i == "" or find_i.downcase == "y" or find_i.downcase == "yes")
+	    puts "Importing Templated Findings template_findings.json..."
 
-    line.each do |j|
-        j["id"] = nil
+	    file = File.new('./templates/template_findings.json',"rb")
+	    json = ""
 
-        finding = TemplateFindings.first(:title => j["title"])
+	    while(line_j = file.gets)
+	        json = json + line_j
+	    end
+	    line = JSON.parse(json)
 
-        j["approved"] = true
-        f = TemplateFindings.first_or_create(j)
-        f.save
-    end
+	    line.each do |j|
+	        j["id"] = nil
+
+	        finding = TemplateFindings.first(:title => j["title"])
+
+	        j["approved"] = true
+	        f = TemplateFindings.first_or_create(j)
+	        f.save
+	    end
+	else
+	    puts "Skipping templated finding import. Use the UI to import templated findings."
+	end
 else
-    puts "Skipping templated finding import. Use the UI to import templated findings."
+	 puts "in quiet mode, skipping importing templates"
 end
 
 # add the Default templates into the DB
@@ -108,28 +132,6 @@ if !templates
 	datax["report_type"] = "Default Template - DREAD Scoring"
 	report = Xslt.new(datax)
 	report.save
-
-	puts "Adding the Default CVSS Report Template"
-	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
-	docx = "./templates/CVSS_Template.docx"
-
-	xslt = generate_xslt(docx)
-	if xslt =~ /Error file DNE/
-		return "ERROR!!!!!!"
-	end
-
-	# open up a file handle and write the attachment
-	File.open(xslt_file, 'wb') {|f| f.write(xslt) }
-
-	# delete the file data from the attachment
-	datax = Hash.new
-	datax["docx_location"] = "#{docx}"
-	datax["xslt_location"] = "#{xslt_file}"
-	datax["description"] = 	"Default CVSS Report"
-	datax["report_type"] = "Default CVSS Report"
-	report = Xslt.new(datax)
-	report.save
-
 
 	puts "Adding the Default CVSSv3 Report Template"
 	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
