125202f96c8e36431dbf125a7273dbf8c0be7bfb
BuffaloWill <wvandevanter@gmail.com>
2017-09-05 09:05:51 -0400

    Increased import limits, https://github.com/SerpicoProject/Serpico/issues/338

--- routes/report.rb
+++ routes/report.rb
@@ -1475,8 +1475,8 @@ post '/report/import' do
     redirect to("/report/import") unless params[:file]
 
     # reject if the file is above a certain limit
-    if params[:file][:tempfile].size > 1000000
-        return "File too large. 1MB limit"
+    if params[:file][:tempfile].size > 100000000
+        return "File too large. 100MB limit"
     end
 
     json_file = params[:file][:tempfile].read
