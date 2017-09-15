df934e69390d78e6b72164ef8bf4d5105504307e
frisch-raphael <frisch.raphael@gmail.com>
2017-09-15 12:14:23 +0200

    trying to fix some of the indentation issue

--- routes/admin.rb
+++ routes/admin.rb
@@ -429,23 +429,23 @@ post '/admin/templates/add' do
 
     @admin = true
 
-	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
+    xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
 
     redirect to("/admin/templates/add") unless params[:file]
 
-	# reject if the file is above a certain limit
-	if params[:file][:tempfile].size > 100000000
-		return "File too large. 10MB limit"
-	end
+    # reject if the file is above a certain limit
+    if params[:file][:tempfile].size > 100000000
+        return "File too large. 10MB limit"
+    end
 
-	docx = "./templates/#{rand(36**36).to_s(36)}.docx"
-	File.open(docx, 'wb') {|f| f.write(params[:file][:tempfile].read) }
+    docx = "./templates/#{rand(36**36).to_s(36)}.docx"
+    File.open(docx, 'wb') {|f| f.write(params[:file][:tempfile].read) }
 
     error = false
     detail = ""
     begin
-	    xslt = generate_xslt(docx)
-		xslt_components = generate_xslt_components(docx)
+        xslt = generate_xslt(docx)
+        xslt_components = generate_xslt_components(docx)
     rescue ReportingError => detail
         error = true
     end
@@ -455,17 +455,17 @@ post '/admin/templates/add' do
         "The report template you uploaded threw an error when parsing:<p><p> #{detail.errorString}"
     else
 
-    	# open up a file handle and write the attachment
-	    File.open(xslt_file, 'wb') {|f| f.write(xslt) }
+      # open up a file handle and write the attachment
+      File.open(xslt_file, 'wb') {|f| f.write(xslt) }
       #extract the screenshot names from the file
       screenshot_names = xslt.scan(/\[!!(.*?)!!]/)
-	    # delete the file data from the attachment
-	    datax = Hash.new
-	    # to prevent traversal we hardcode this
-	    datax["docx_location"] = "#{docx}"
-	    datax["xslt_location"] = "#{xslt_file}"
-	    datax["description"] = 	params[:description]
-	    datax["report_type"] = params[:report_type]
+      # delete the file data from the attachment
+      datax = Hash.new
+      # to prevent traversal we hardcode this
+      datax["docx_location"] = "#{docx}"
+      datax["xslt_location"] = "#{xslt_file}"
+      datax["description"] = 	params[:description]
+      datax["report_type"] = params[:report_type]
       datax["screenshot_names"] = screenshot_names.join(",")
       data = url_escape_hash(datax)
       data["finding_template"] = params[:finding_template] ? true : false
@@ -475,32 +475,32 @@ post '/admin/templates/add' do
 
       if @template
           @template.update(:xslt_location => data["xslt_location"], :docx_location => data["docx_location"], :description => data["description"], :screenshot_names => data["screenshot_names"])
-			@template.components.destroy
-	    else
-		    @template = Xslt.new(data)
-		    @template.save
-	    end
+	  @template.components.destroy
+      else
+          @template = Xslt.new(data)
+          @template.save
+      end
 
-		# create a xslt file for each component
-		list_components_files = []
-		xslt_components.each do |component_name, component_xslt|
-			componentHash = Hash.new
-			componentHash['xslt_location'] = "./templates/#{rand(36**36).to_s(36)}.xslt"
-			componentHash['name'] = component_name
-			componentHash['xslt'] = @template
-			File.open(componentHash['xslt_location'], 'wb') {|f| f.write(component_xslt) }
-			list_components_files.push(componentHash)
-		end
+      # create a xslt file for each component
+      list_components_files = []
+      xslt_components.each do |component_name, component_xslt|
+          componentHash = Hash.new
+          componentHash['xslt_location'] = "./templates/#{rand(36**36).to_s(36)}.xslt"
+          componentHash['name'] = component_name
+          componentHash['xslt'] = @template
+          File.open(componentHash['xslt_location'], 'wb') {|f| f.write(component_xslt) }
+          list_components_files.push(componentHash)
+      end
 
-		# insert components into the db
-		list_components_files.each do |component|
-			@component = Xslt_component.new(component)
-			@component.save
-		end
-	    redirect to("/admin/templates")
+      # insert components into the db
+      list_components_files.each do |component|
+          @component = Xslt_component.new(component)
+          @component.save
+      end
+      redirect to("/admin/templates")
 
-        haml :add_template, :encode_html => true
-    end
+      haml :add_template, :encode_html => true
+  end
 end
 
 # Manage Templated Reports
